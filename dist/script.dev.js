ae0f87dd98338c0efd3b76f2a370b6749c579b12
Rafael Ramires <moreira.ramires@gmail.com>
2021-02-13 22:40:39 -0300

    Styles completed

---
+++ dist/script.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var grids = document.querySelectorAll('.grid');
+var headings = document.querySelectorAll('.heading .wrapper .text');
+
+function enterScreen(index) {}
+
+function exitScreen(index, exitDelay) {}
+
+function setupAnimationCycle(_ref) {
+  var initialScreenIndex = _ref.initialScreenIndex,
+      timePerScreen = _ref.timePerScreen,
+      exitDelay = _ref.exitDelay;
+}
+
+setupAnimationCycle();
---
+++ script.js
@@ -0,0 +1,14 @@
+const grids = document.querySelectorAll('.grid');
+const headings = document.querySelectorAll('.heading .wrapper .text');
+
+function enterScreen(index){
+
+}
+
+function exitScreen( index, exitDelay){
+
+}
+
+function setupAnimationCycle({initialScreenIndex, timePerScreen, exitDelay}){}
+
+setupAnimationCycle()
